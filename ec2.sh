283a0eb1121d507ed53b961a32e973d6ef7850e2
usman6745 <33342456+usman6745@users.noreply.github.com>
2019-03-12 13:23:01 +0530

    Create ec2.sh

---
+++ ec2.sh
@@ -0,0 +1,7 @@
+aws ec2 run-instances --image-id $ami_id --count 1 --instance-type t2.micro --key-name $key_name --security-group-ids sg-088974251af4f8415 --subnet-id subnet-0b57a25fd5a0448d0 --region ap-south-1  > ec2.txt
+grep InstanceId ec2.txt | tr -d '", "' > InstanceId
+sed -i 's/:/=/g' InstanceId
+echo "aws ec2 create-tags --resources ""$"InstanceId"  --tags Key=Name,Value=Web1 --region ap-south-1" >> InstanceId
+chmod +x InstanceId
+./InstanceId
+echo " Instance is launched"
